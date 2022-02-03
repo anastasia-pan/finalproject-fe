1142a92956a201197d54a9d76d02a1f1f8dae3db
Anastasia <panoutsou.anastasia@gmail.com>
2022-02-03 17:52:40 +0000

    added log out button, looks hideous

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
-
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import creepy from "../assets/creepy.png"
+import creepy from "../assets/creepy.png";
 import Login from "./Login";
-function Navbar( { user, setUser } ) {
+function Navbar({ user, setUser }) {
+  const logOut = () => {
+    setUser(null);
+  };
+
   return (
     <div className="navBar">
       <img src={creepy} alt="creepy curios" className="logo" />
@@ -18,13 +21,20 @@ function Navbar( { user, setUser } ) {
               Find Objects
             </Link>
           </li>
-          
 
           <li>
-              {user ?  <Link to="/profile" className="middle">
-              Profile
-            </Link> : <Login user={user} setUser={setUser} /> } 
-            
+            {user ? (
+              <>
+                <Link to="/profile" className="middle">
+                  Profile
+                </Link>
+                <button type="submit" onClick={logOut}>
+                  Log out
+                </button>
+              </>
+            ) : (
+              <Login user={user} setUser={setUser} />
+            )}
           </li>
 
           {/* <li><Login user={user} setUser={setUser}/></li> */}
